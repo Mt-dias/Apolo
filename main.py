cd16efdccc4a88528b9c673e853d4f05c5e14a4e
Mt-dias <matheusdiassss.123e@gmail.com>
2022-09-01 20:20:17 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Our main file.
+
+import speech_recognition as sr
+
+# Criar um Reconhecedor 
+r = sr.Recognizer()
+
+# Abrir o Microfone para captura
+with sr.Microphone() as source:
+   audio = r.listen(source) # Define microfone como fonte de áudio
+
+   print(r.recognize_google(audio)) 
+
+
+
+
